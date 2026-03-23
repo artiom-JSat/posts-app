a75d5537a64b70e54394994f8345401e827289c6
Artiom <jsat1987@gmail.com>
2026-03-23 16:00:32 +0100

    fix: update `Label` and `Separator` imports to use internal `pkg/theme/ui` paths.

--- src/pkg/theme/ui/field.tsx
+++ src/pkg/theme/ui/field.tsx
@@ -4,8 +4,8 @@ import { useMemo } from 'react'
 import { cva, type VariantProps } from 'class-variance-authority'
 
 import { cn } from 'pkg/theme/lib/utils'
-import { Label } from '@/shared/ui/label/label.component'
-import { Separator } from '@/shared/ui/separator'
+import { Label } from 'pkg/theme/ui/label'
+import { Separator } from 'pkg/theme/ui/separator'
 
 function FieldSet({ className, ...props }: React.ComponentProps<'fieldset'>) {
   return (
